0a9af41381d53393f4ff4bc65c751b0847f61c5d
Ploinky <mail@ploinky.de>
2025-11-08 17:08:00 +0100

    except for the missing shaders, it starts! it does nothing, but it also does not crash

--- client/src/core/graphics/opengl-graphics-engine.cpp
+++ client/src/core/graphics/opengl-graphics-engine.cpp
@@ -6,93 +6,112 @@ IGraphicsEngine* IGraphicsEngine::Create(HWindow hWindow, int nWidth, int nHeigh
 
 
 HShaderProgram COpenGLGraphicsEngine::LoadShaderProgram(std::string strShaderName, EVertexFormat eVertexFormat, std::vector<uint8_t> vecVsBytecode, std::vector<uint8_t> vecPsBytecode) {
-    
+    return INVALID_HANDLE;
 }
 HBitmap COpenGLGraphicsEngine::LoadBitmapImage(unsigned char* pImageData, int uWidth, int uHeight) {
-    
+    return INVALID_ASSET_HANDLE;
 }
 HTexture COpenGLGraphicsEngine::LoadTexture(unsigned char* pImageData, int uWidth, int uHeight) {
-    
+    return INVALID_ASSET_HANDLE;
 }
 
 // == buffer management
 BufferHandle_t COpenGLGraphicsEngine::CreateVertexBuffer(void* pVertices, size_t uSize, int nCount) {
-    
+    return BufferHandle_t {.ptr = nullptr};
 }
 BufferHandle_t COpenGLGraphicsEngine::CreateIndexBuffer(uint32_t* pIndices, int nCount) {
-    
+    return BufferHandle_t {.ptr = nullptr};
 }
 BufferHandle_t COpenGLGraphicsEngine::CreateConstantBuffer(size_t uSize, void* pInitialData) {
-    
+    return BufferHandle_t {.ptr = nullptr};
 }
 BufferHandle_t COpenGLGraphicsEngine::CreateInstanceBuffer(void* instances, int instance_count, size_t size) {
-    
+    return BufferHandle_t {.ptr = nullptr};
 }
 void COpenGLGraphicsEngine::UpdateBuffer(BufferHandle_t hBuffer, const void* pData, size_t uSize) {
-    
+
 }
 
 // == window events?
 void COpenGLGraphicsEngine::SetWindowDimensions(int nWidth, int nHeight) {
-    
+
 }
 void COpenGLGraphicsEngine::SetFullScreen(bool bFullscreen) {
-    
+
 }
 
 // == drawing
 void COpenGLGraphicsEngine::ClearScreen() {
-    
+
 }
 
 void COpenGLGraphicsEngine::BindVertexShaderConstantBuffer(int nSlot, BufferHandle_t hBuffer) {
-    
+
 }
 
 void COpenGLGraphicsEngine::EnableAlphaBlending() {
-    
+
 }
 void COpenGLGraphicsEngine::DisableAlphaBlending() {
-    
+
 }
 
 void COpenGLGraphicsEngine::EnableDepthStencilState() {
-    
+
 }
 void COpenGLGraphicsEngine::DisableDepthStencilState() {
-    
+
 }
 
 
 void COpenGLGraphicsEngine::BindShaderProgram(HShaderProgram hProgram) {
-    
+
 }
 void COpenGLGraphicsEngine::BindSampler(uint32_t uSlot, HSampler hSampler) {
-    
+
 }
 void COpenGLGraphicsEngine::BindTexture(uint32_t uSlot, HTexture hTexture) {
-    
+
 }
 
 void COpenGLGraphicsEngine::SetVertexBuffer(uint32_t uSlot, BufferHandle_t& vertexBuffer, unsigned int uStride, unsigned int uOffset) {
-    
+
 }
 void COpenGLGraphicsEngine::SetIndexBuffer(BufferHandle_t& indexBuffer) {
-    
+
 }
 
 void COpenGLGraphicsEngine::DrawIndexed(unsigned int indices) {
-    
+
 }
 void COpenGLGraphicsEngine::DrawInstanced(unsigned int uVertexCountPerInstance, unsigned int uInstanceCount) {
-    
+
 }
 
 bool COpenGLGraphicsEngine::Present() {
-    
+    return true;
 }
 
 // == 2d
-ICanvas2D*COpenGLGraphicsEngine:: GetCanvas2D() {
-    
+ICanvas2D* COpenGLGraphicsEngine:: GetCanvas2D() {
+    return this;
+}
+
+void COpenGLGraphicsEngine::RenderText(int x, int y, int w, int h, float color[3], std::string strText) {
+
+}
+void COpenGLGraphicsEngine::DrawRect(int x, int y, int w, int h, float color[3]) {
+
+}
+void COpenGLGraphicsEngine::DrawShape(Vector2* points, int pointCount, float color[3]) {
+
+}
+void COpenGLGraphicsEngine::FillShape(Vector2* points, int pointCount, float color[3]) {
+
+}
+void COpenGLGraphicsEngine::FillRect(int x, int y, int w, int h, float color[3]) {
+
+}
+void COpenGLGraphicsEngine::DrawImage(float x, float y, float w, float h, HBitmap hBmp) {
+
 }
--- client/src/core/graphics/opengl-graphics-engine.h
+++ client/src/core/graphics/opengl-graphics-engine.h
@@ -1,47 +1,54 @@
 #include <core/graphics/graphics-engine.h>
 
-class COpenGLGraphicsEngine : public IGraphicsEngine {
+class COpenGLGraphicsEngine : public IGraphicsEngine, ICanvas2D {
 public:
 
-    HShaderProgram LoadShaderProgram(std::string strShaderName, EVertexFormat eVertexFormat, std::vector<uint8_t> vecVsBytecode, std::vector<uint8_t> vecPsBytecode) override;
-    HBitmap LoadBitmapImage(unsigned char* pImageData, int uWidth, int uHeight) override;
-    HTexture LoadTexture(unsigned char* pImageData, int uWidth, int uHeight) override;
+    virtual HShaderProgram LoadShaderProgram(std::string strShaderName, EVertexFormat eVertexFormat, std::vector<uint8_t> vecVsBytecode, std::vector<uint8_t> vecPsBytecode) override;
+    virtual HBitmap LoadBitmapImage(unsigned char* pImageData, int uWidth, int uHeight) override;
+    virtual HTexture LoadTexture(unsigned char* pImageData, int uWidth, int uHeight) override;
 
     // == buffer management
-    BufferHandle_t CreateVertexBuffer(void* pVertices, size_t uSize, int nCount) override;
-    BufferHandle_t CreateIndexBuffer(uint32_t* pIndices, int nCount) override;
-    BufferHandle_t CreateConstantBuffer(size_t uSize, void* pInitialData) override;
-    BufferHandle_t CreateInstanceBuffer(void* instances, int instance_count, size_t size) override;
-    void UpdateBuffer(BufferHandle_t hBuffer, const void* pData, size_t uSize) override;
+    virtual BufferHandle_t CreateVertexBuffer(void* pVertices, size_t uSize, int nCount) override;
+    virtual BufferHandle_t CreateIndexBuffer(uint32_t* pIndices, int nCount) override;
+    virtual BufferHandle_t CreateConstantBuffer(size_t uSize, void* pInitialData) override;
+    virtual BufferHandle_t CreateInstanceBuffer(void* instances, int instance_count, size_t size) override;
+    virtual void UpdateBuffer(BufferHandle_t hBuffer, const void* pData, size_t uSize) override;
     
     // == window events?
-	void SetWindowDimensions(int nWidth, int nHeight) override;
-	void SetFullScreen(bool bFullscreen) override;
+	virtual void SetWindowDimensions(int nWidth, int nHeight) override;
+	virtual void SetFullScreen(bool bFullscreen) override;
 
     // == drawing
-    void ClearScreen() override;
+    virtual void ClearScreen() override;
 
-    void BindVertexShaderConstantBuffer(int nSlot, BufferHandle_t hBuffer) override;
+    virtual void BindVertexShaderConstantBuffer(int nSlot, BufferHandle_t hBuffer) override;
     
-    void EnableAlphaBlending() override;
-    void DisableAlphaBlending() override;
+    virtual void EnableAlphaBlending() override;
+    virtual void DisableAlphaBlending() override;
     
-    void EnableDepthStencilState() override;
-    void DisableDepthStencilState() override;
+    virtual void EnableDepthStencilState() override;
+    virtual void DisableDepthStencilState() override;
 
     
-    void BindShaderProgram(HShaderProgram hProgram) override;
-    void BindSampler(uint32_t uSlot, HSampler hSampler) override;
-    void BindTexture(uint32_t uSlot, HTexture hTexture) override;
+    virtual void BindShaderProgram(HShaderProgram hProgram) override;
+    virtual void BindSampler(uint32_t uSlot, HSampler hSampler) override;
+    virtual void BindTexture(uint32_t uSlot, HTexture hTexture) override;
 
-    void SetVertexBuffer(uint32_t uSlot, BufferHandle_t& vertexBuffer, unsigned int uStride, unsigned int uOffset) override;
-    void SetIndexBuffer(BufferHandle_t& indexBuffer) override;
+    virtual void SetVertexBuffer(uint32_t uSlot, BufferHandle_t& vertexBuffer, unsigned int uStride, unsigned int uOffset) override;
+    virtual void SetIndexBuffer(BufferHandle_t& indexBuffer) override;
 
-    void DrawIndexed(unsigned int indices) override;
-    void DrawInstanced(unsigned int uVertexCountPerInstance, unsigned int uInstanceCount) override;
+    virtual void DrawIndexed(unsigned int indices) override;
+    virtual void DrawInstanced(unsigned int uVertexCountPerInstance, unsigned int uInstanceCount) override;
 
-    bool Present() override;
+    virtual bool Present() override;
 
     // == 2d
-    ICanvas2D* GetCanvas2D() override;
+    virtual ICanvas2D* GetCanvas2D() override;
+
+    virtual void RenderText(int x, int y, int w, int h, float color[3], std::string strText) override;
+    virtual void DrawRect(int x, int y, int w, int h, float color[3]) override;
+    virtual void DrawShape(Vector2* points, int pointCount, float color[3]) override;
+    virtual void FillShape(Vector2* points, int pointCount, float color[3]) override;
+    virtual void FillRect(int x, int y, int w, int h, float color[3]) override;
+    virtual void DrawImage(float x, float y, float w, float h, HBitmap hBmp) override;
 };
