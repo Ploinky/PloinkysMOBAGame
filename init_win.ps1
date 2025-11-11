b3dcdf23be01edd6de4b6a9596ac1b1ecd37a122
ploinky <mail@ploinky.de>
2025-11-11 13:22:24 +0100

    refactor assets?

--- init_win.ps1
+++ init_win.ps1
@@ -1,10 +1,1 @@
-Write-Output "Fetching dependencies..."
-
-Write-Output "Downloading and unpacking GLFW 3.4"
-Invoke-WebRequest -L https://github.com/glfw/glfw/releases/download/3.4/glfw-3.4.bin.WIN64.zip -o test.zip
-New-Item -Force -Path '.' -Name 'temp_glfw' -ItemType "Directory" | Out-Null
-Compress-Archive -xzf test.zip -C temp_glfw/
-Copy-Item -R -Force temp_glfw/glfw-3.4.bin.WIN64/include/* vendor/include/
-Copy-Item -R -Force temp_glfw/glfw-3.4.bin.WIN64/lib-vc2022/* vendor/lib/
-Remove-Item -Force -Recurse temp_glfw
-Remove-Item -Force test.zip
+New-Item -ItemType SymbolicLink -Path .\_out\data\ -Target .\assets_raw\
