6cb507be6e8c1de41ddc9c389139e8db6d54964a
ploinky <mail@ploinky.de>
2025-11-12 08:10:37 +0100

    fix damage being applied twice on spell hit, hide nav mesh

--- apps/server/src/spell-cast-api.cpp
+++ apps/server/src/spell-cast-api.cpp
@@ -10,14 +10,5 @@ CSpellCastApi::CSpellCastApi(CGameState* pGameState) {
 
 void CSpellCastApi::ApplyDamage(UnitId idSource, UnitId idTarget, float fDamage) {
     CGameObject* pTarget = m_pGameState->FindGameObjectById(idTarget);
-
-    CHealthComponent* pHealth = pTarget->GetComponent<CHealthComponent>();
-
-    if(pHealth == nullptr) {
-        return;
-    }
-
-    pHealth->nHealth -= fDamage;
-
     m_pGameState->VecEvent.emplace(new CDamageEvent(idSource, idTarget, fDamage));
 }
